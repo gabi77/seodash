0c1ed715320b0849ecc61a74713c214463ae1369
Gabriel Janez <gabriel_janeez@hotmail.com>
2019-03-20 08:11:26 +0100

    Code Review & Resolve Error security:check sensio

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
